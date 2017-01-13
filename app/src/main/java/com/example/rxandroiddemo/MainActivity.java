114a90b11793a44c73d6dca2d7163fd9ce482df4
wangyuhang <steven.yuhang.wang@gmail.com>
2017-01-13 16:14:22 +0800

    first commit

---
+++ app/src/main/java/com/example/rxandroiddemo/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.rxandroiddemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gotoSimpleModule();
+    }
+
+    // 跳转简单的页面
+    public void gotoSimpleModule() {
+        startActivity(new Intent(this, SimpleActivity.class));
+    }
+
+    // 跳转复杂的页面
+    public void gotoMoreModule() {
+        startActivity(new Intent(this, MoreActivity.class));
+    }
+}
